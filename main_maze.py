3e7a49698d2300cff98ced7436ec2098c6d44f7f
victorjtn <josev2096@gmail.com>
2025-08-05 21:26:45 -0300

    att

---
+++ main_maze.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import time
+from maze import Maze
+from collections import deque
+
+s = deque()
+
+
+maze_csv_path = "labirinto1.txt"  # Substitua pelo caminho correto
+maze = Maze() 
+
+maze.load_from_csv(maze_csv_path)
+
+# Exibir o lab
+maze.run()
+maze.init_player()
+
+p = maze.get_init_pos_player()
+
+maze.mov_player(p)
+
+
